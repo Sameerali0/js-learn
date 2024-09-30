2bc46f20ece7d3a38bc0f79e49d4074f0588361a
Sameer ali <sameer.ali.professional@gmail.com>
2024-09-30 13:36:13 +0000

    create first class files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "sameer@google.com"
+var accouuntPassword = 12345
+accountCity = "karachi"
+
+// accountId = 2 // not allowed
+
+accountEmail = "sc@sc.com"
+accouuntPassword = "212121"
+accountCity = "lahore"
+
+console.log(accountId);
+
+// console.table([accountId, accountEmail, accouuntPassword, accountCity])
